c784f26df3b8759cb2577c6f282fa5a0cea01866
TANG CHENG LEE <TANGCHENGLEE@virgil246.com>
2024-07-08 20:36:47 +0800

    Merge branch 'frontend'

---
+++ Unnotech_front/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <Table class="mx-10">
+        <TableHeader>
+            <TableRow>
+                <TableHead class="w-[30%]">
+                    Thumbnail
+                </TableHead>
+                <TableHead class="w-[70%]">Title</TableHead>
+
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+
+            <TableRow @click="handleOnClick(id)" v-for="(item, id) in data" :id="id">
+                <TableCell>
+                    <img :src="item.news_photo.imgUrl" alt="" class="h-[150px]">
+                </TableCell>
+                <TableCell class='text-xl'>{{ item.title }}
+                </TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+</template>
+
+<script setup lang="ts">
+import data from "~/assets/example.json";
+
+
+const router = useRouter();
+const handleOnClick = (id) => {
+
+    console.log(id)
+    router.push({ path: `/nba/${id}` })
+}
+</script>
---
+++ Unnotech_front/pages/nba/[id].vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div class="px-[10%] py-[1rem]">
+        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            {{ posts.full_title }}
+        </h1>
+        <div class="content" v-html="posts.content">
+        </div>
+
+    </div>
+
+</template>
+<script lang="ts" setup>
+import data from "~/assets/example.json";
+
+const { params: { id } } = useRoute();
+const posts = computed(() => data[id])
+
+</script>
+
+<style scoped>
+.content {
+    :deep(p) {
+        line-height: 1.75rem;
+        margin-top: 30px;
+        text-indent: 1em;
+
+    }
+
+    :deep(.embedded-content) {
+        margin-left: auto;
+        margin-right: auto;
+        width: 500px
+    }
+
+    :deep(.video-container),
+    :deep(.photo_center) {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 80vw
+    }
+
+
+
+    :deep(.embedded-content+p+p) {
+        text-align: center;
+        display: none;
+
+    }
+}
+</style>

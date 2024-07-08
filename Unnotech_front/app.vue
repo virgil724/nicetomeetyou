1e83a74b8d1ff27ac83d0d7e70b8f165ab9ab58a
TANG CHENG LEE <TANGCHENGLEE@virgil246.com>
2024-07-09 01:01:13 +0800

    add WebSocket Frontend

--- Unnotech_front/app.vue
+++ Unnotech_front/app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-   <NuxtPage />
+    <NuxtPage />
   </div>
+  <Toaster />
 </template>
+<script lang="ts" setup>
+import { useToast } from '@/components/ui/toast/use-toast'
+const { toast } = useToast()
+const popToast = (description: string) => {
+  console.log('click')
+  toast({
+    title: '有新新聞',
+    description: description,
+  });
+}
+const ws = new WebSocket("ws://localhost:8000/ws/nba/")
+ws.onopen = function () {
+  ws.send(JSON.stringify({
+    action: "subscribe_to_news_activity",
+    request_id: new Date().getTime(),
+  }))
+}
+ws.onmessage = function (e) {
+  const { title } = JSON.parse(e.data)
+  popToast(title)
+}
+
+
+</script>
